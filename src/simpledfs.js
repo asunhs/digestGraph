dc9fec86b59ece5243d184171332134f0e5185c7
Hyunsang <asunhs@gmail.com>
2015-01-27 00:48:26 +0900

    edge

--- src/simpledfs.js
+++ src/simpledfs.js
@@ -5,9 +5,9 @@
  * @returns {Vertex}
  * @constructor
  */
-function Vertex (id, neighbors) {
+function Vertex (id) {
     if (!(this instanceof Vertex)) {
-        return new Vertex(id, neighbors);
+        return new Vertex(id);
     }
 
     if (!id) {
@@ -15,7 +15,8 @@ function Vertex (id, neighbors) {
     }
 
     this.id = id;
-    this.neighbors = neighbors || [];
+    this.neighbors = [];
+    this.edges = [];
     this.visited = false;
 }
 
@@ -26,12 +27,32 @@ Vertex.prototype.link = function (vertex) {
 
 Vertex.prototype.unlink = function (vertex) {
     if (this.neighbors.indexOf(vertex) > 0) {
-        this.neighbors.splice(this.neighbors.indexOf(vertex), 0);
+        this.neighbors.splice(this.neighbors.indexOf(vertex), 1);
     }
 
     if (vertex.neighbors.indexOf(this) > 0) {
-        vertex.neighbors.splice(vertex.neighbors.indexOf(this), 0);
+        vertex.neighbors.splice(vertex.neighbors.indexOf(this), 1);
     }
+};
+
+Vertex.prototype.add = function (edge) {
+    var opposite = edge.opposite(this);
+    this.edges.push(edge);
+    opposite.edges.push(this);
+    this.link(opposite);
+};
+
+Vertex.prototype.remove = function (edge) {
+    if (this.edges.indexOf(edge) > 0) {
+        this.edges.splice(this.edges.indexOf(edge), 1);
+    }
+    var opposite = edge.opposite(this);
+
+    if (opposite.edges.indexOf(edge) > 0) {
+        opposite.edges.splice(opposite.edges.indexOf(edge), 1);
+    }
+
+    this.unlink(opposite);
 };
 
 Vertex.prototype.visit = function () {
@@ -42,21 +63,53 @@ Vertex.prototype.leave = function () {
     this.visited = false;
 };
 
-function SimpleDFS (vertexes) {
-    if (!(this instanceof SimpleDFS)) {
-        return new SimpleDFS(vertexes);
+
+
+function Edge (lhs, rhs, weight) {
+    if (!(this instanceof Edge)) {
+        return new Edge(lhs, rhs, weight);
     }
 
-    this.graph = vertexes;
+    this.weight = weight;
+    this.vertexes = [lhs, rhs];
+    lhs.add(this);
+}
+
+Edge.prototype.is = function (vertex) {
+    return this.vertexes[0] === vertex || this.vertexes[1] === vertex;
+};
+
+Edge.prototype.opposite = function (vertex) {
+    return this.vertexes[0] === vertex ? this.vertex[1] : this.vertex[0];
+};
+
+
+
+
+
+
+
+function SimpleDFS (edges) {
+    if (!(this instanceof SimpleDFS)) {
+        return new SimpleDFS(edges);
+    }
+
+    this.edges = edges;
+    this.vertexes = _.union.apply(_, _.map(edges, function (edge) {
+        return edge.vertexes;
+    }));
 }
 
 
 function findCycle (path) {
 
     var last = _.last(path),
-        initial = _.initial(path);
+        initial = _.initial(path),
+        cycle = path.slice(initial.lastIndexOf(last));
 
-    return console.table(initial.slice(initial.lastIndexOf(last)));
+
+
+    return console.table(cycle);
 }
 
 
@@ -82,7 +135,7 @@ function digest (vertex, path) {
 }
 
 SimpleDFS.prototype.digest = function () {
-    _.each(this.graph, function (vertex) {
+    _.each(this.vertexes, function (vertex) {
         digest(vertex);
     });
 }
